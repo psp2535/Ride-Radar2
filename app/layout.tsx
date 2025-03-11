d25c0a50cc1ce93f6e5437bca4ef4cf70e5edc3b
psp2535 <saipranav974@gmail.com>
2025-03-11 14:27:43 +0530

    added globals

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
 import "./styles.css";
-import "./global.css";
+import "./globals.css";
+import "@/app/global.css";
 
 
 const geistSans = Geist({
